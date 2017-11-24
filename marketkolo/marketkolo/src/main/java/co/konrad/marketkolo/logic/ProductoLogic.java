09f420f5c6db6d8b8ab4737e861459d04d1b11f6
Karol Campo Vives <karcav11@gmail.com>
2017-11-23 20:49:49 -0500

    Creación de método para obtener productos por su id Creación de método para crear productos Creación de método para actualizar productos Creación de método para eliminar productos usando como parámetro su id

--- marketkolo/marketkolo/src/main/java/co/konrad/marketkolo/logic/ProductoLogic.java
+++ marketkolo/marketkolo/src/main/java/co/konrad/marketkolo/logic/ProductoLogic.java
@@ -30,8 +30,45 @@ public class ProductoLogic {
         return productos;
     }
     
+    /**
+     * Obtener un producto a partir de su id
+     * @param idProducto
+     * @return producto
+     */
+    public ProductoEntity getProducto(Long idProducto){
+        ProductoEntity producto = productoPersistence.find(idProducto);
+        if(producto == null){
+            throw new IllegalArgumentException("El producto solicitado no existe");
+        }
+        return producto;
+    }
     
+    /**
+     * Crear un nuevo producto
+     * @param entity
+     * @return entity
+     */
+    public ProductoEntity createProducto(ProductoEntity entity){
+        productoPersistence.create(entity);
+        return entity;
+    }
     
+    /**
+     * Método para actualizar un objeto de la entidad producto
+     * @param idProducto
+     * @param entity
+     * @return producto
+     */
+    public ProductoEntity updateProducto (Long idProducto, ProductoEntity entity){
+        ProductoEntity producto = productoPersistence.update(entity);
+        return producto;
+    }
     
-    
+    /**
+     * Metodo para eliminar un objeto de la entidad Producto
+     * @param idProducto
+     */
+    public void deleteProducto(Long idProducto){
+        productoPersistence.delete(idProducto);
+    }
 }
